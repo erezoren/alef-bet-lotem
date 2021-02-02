97370366fdf1f8c3327a29a3d9363d0576a1949c
eoren <eoren@ebay.com>
2021-02-02 19:37:03 +0200

    keepalive

--- index.js
+++ index.js
@@ -2,20 +2,16 @@ const express = require('express');
 const path = require('path');
 const topicsApi = require('./src/server/routs/topics');
 const gameApi = require('./src/server/routs/game');
-const repo=require('./src/server/repository/redisClient')
+const repo = require('./src/server/repository/redisClient')
 var socketServer = require('./socketserver');
 const app = express();
 var cors = require('cors')
 const axios = require('axios');
 const cron = require('node-cron');
 
-
-
-
 app.use(express.static(__dirname + '/dist'));
 app.use(express.json());
 app.use(cors());
-
 
 app.get('/api/', (req, res) => {
   res.json({erez: "oren2"});
@@ -26,8 +22,11 @@ app.use('/api/games', gameApi);
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('/error', (req, res) => {
-  console.error(JSON.stringify(res));
   res.sendFile(path.join(__dirname + '/src/client/build/index.html'));
+});
+
+app.get('/ack', (req, res) => {
+  console.log("Refreshing.....")
 });
 
 const port = process.env.PORT || 8080;
@@ -41,12 +40,11 @@ repo.init();
 
 console.log(`listening on ${port}`);
 
-
 cron.schedule('* * * * *', async () => {
-  console.log('Refreshing application');
+  console.log(`Refreshing application http://localhost:${port}/ack`);
   axios
   .get(
-      `https://alefbetlotem.herokuapp.com/`)
+      `http://localhost:${port}/ack`)
   .then(response => {
     console.info("Done refreshing application")
   })
